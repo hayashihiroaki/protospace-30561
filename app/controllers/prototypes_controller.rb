7f560222f9dee38d6c6c6bd0524454aed4f83102
hayashihiroaki <61njn0@gmail.com>
2020-09-27 14:12:32 +0900

    ユーザー詳細ページ,未ログイン時のページ遷移修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:edit, :update, :destroy,]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy,:new]
   before_action :move_to_index, except: [:index, :show, :new, :create]
 
 
