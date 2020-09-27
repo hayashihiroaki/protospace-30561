e20ab5e326c5921eb5066b2df85167039e2e5c12
hayashihiroaki <61njn0@gmail.com>
2020-09-27 12:53:16 +0900

    prototype_controller修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :update, :destroy,]
-  before_action :move_to_index, except: [:index, :show, :new]
+  before_action :move_to_index, except: [:index, :show, :new, :create]
 
 
   def index
@@ -14,7 +14,7 @@ class PrototypesController < ApplicationController
     if @prototype.save
       redirect_to root_path
     else
-      render :new_prototype_path
+      render :new
     end
   end
 
@@ -51,7 +51,9 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    if user_signed_in? 
+    @prototype = Prototype.find(params[:id])
+  
+    unless user_signed_in? && current_user.id == @prototype.user_id
       redirect_to action: :index
     end
   end
